5d67fc7c136bba8c87b0909c1ef3ab2573201c69
Andrea Bodini <andreabodo09@gmail.com>
2020-11-22 14:29:25 +0100

    Pulizia codice FileManager

--- OperationsBetweenForests/Serialization/FileManager.cs
+++ OperationsBetweenForests/Serialization/FileManager.cs
@@ -2,76 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using YAXLib;
 using System.Text.Json;
 using Microsoft.Win32;
-using OperationsBetweenForests.Models;
 using OperationsBetweenForests.Core;
 
 namespace OperationsBetweenForests.Serialization
 {
     public static class FileManager
     {
-        #region Old method
-        [Obsolete]
-        /// <summary>
-        /// Serializes data classes list to file
-        /// </summary>
-        /// <param name="filename">File name</param>
-        /// <param name="modelsList">Data classes list</param>
-        public static void SerializeDataToFile(string filename, List<GraphSerializationData> modelsList)
-        {
-            using (FileStream stream = File.Open(filename, FileMode.Create, FileAccess.Write, FileShare.Read))
-            {
-                SerializeDataToStream(stream, modelsList);
-            }
-        }
-        [Obsolete]
-        /// <summary>
-        /// Deserializes data classes list from file
-        /// </summary>
-        /// <param name="filename">File name</param>
-		public static List<GraphSerializationData> DeserializeDataFromFile(string filename)
-        {
-            using (FileStream stream = File.Open(filename, FileMode.Open, FileAccess.Read, FileShare.Read))
-            {
-                return DeserializeDataFromStream(stream);
-            }
-        }
-        [Obsolete]
-        /// <summary>
-        /// Serializes graph data list to a stream
-        /// </summary>
-        /// <param name="stream">The destination stream</param>
-        /// <param name="modelsList">The graph data</param>
-		public static void SerializeDataToStream(Stream stream, List<GraphSerializationData> modelsList)
-        {
-            var serializer = new YAXSerializer(typeof(List<GraphSerializationData>));
-            using (var textWriter = new StreamWriter(stream))
-            {
-                serializer.Serialize(modelsList, textWriter);
-                textWriter.Flush();
-            }
-        }
-        [Obsolete]
-        /// <summary>
-        /// Deserializes graph data from a stream
-        /// </summary>
-        /// <param name="stream">The stream</param>
-        /// <returns>The graph data</returns>
-		public static List<GraphSerializationData> DeserializeDataFromStream(Stream stream)
-        {
-            var deserializer = new YAXSerializer(typeof(List<GraphSerializationData>));
-            using (var textReader = new StreamReader(stream))
-            {
-                return (List<GraphSerializationData>)deserializer.Deserialize(textReader);
-            }
-        }
-        #endregion
-
         #region Serialization/deserialization to/from json
         /// <summary>
         /// Prende in input un oggetto e ne restituisce la stringa in formato JSON
